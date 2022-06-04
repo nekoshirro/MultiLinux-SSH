0dedf6460dcd3ffedf567e79246854c5dad19450
Sushrut1101 <guptasushrut@gmail.com>
2022-06-04 20:44:48 +0530

    Linux: Refractor code for all linux distros
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- spam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true
-do
-	cat /tmp/ssh.txt
-	sleep 3
-done
--- tmate.sh
+++ tmate.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+cd ~
 tmate -S /tmp/tmate.sock new-session -d               # Launch tmate in a headless mode
 tmate -S /tmp/tmate.sock wait tmate-ready             # Blocks until the SSH connection is established
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'    # Prints the SSH connection string
